ed33e0eef7c4b949e97712a9a4fdc13516dccb6c
MoviniGayaraa <movinigayara32@gmail.com>
2024-05-30 09:19:46 +0530

    content changed

--- Course_Work/src/main/java/lk/ijse/gdse/Controller/CustomerController.java
+++ Course_Work/src/main/java/lk/ijse/gdse/Controller/CustomerController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/v1/customer")
+@RequestMapping("")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class CustomerController {
